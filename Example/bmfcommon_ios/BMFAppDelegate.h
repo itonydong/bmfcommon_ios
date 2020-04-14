ba753fe2cb16a70b71709355c0330b943ab16b84
dongtao03 <dongtao03@baidu.com>
2020-04-14 11:42:21 +0800

    Initial commit

---
+++ Example/bmfcommon_ios/BMFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BMFAppDelegate.h
+//  bmfcommon_ios
+//
+//  Created by tonydong@foxmail.com on 04/14/2020.
+//  Copyright (c) 2020 tonydong@foxmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BMFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/bmfcommon_ios/BMFViewController.m
@@ -0,0 +1,29 @@
+//
+//  BMFViewController.m
+//  bmfcommon_ios
+//
+//  Created by tonydong@foxmail.com on 04/14/2020.
+//  Copyright (c) 2020 tonydong@foxmail.com. All rights reserved.
+//
+
+#import "BMFViewController.h"
+
+@interface BMFViewController ()
+
+@end
+
+@implementation BMFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
